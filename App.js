131fab5308c390a574c06de281a90b9cdc93ec72
Utkarsh gupta <Utkarsh16071998@gmail.com>
2022-12-12 16:52:50 +0530

    set up

---
+++ App.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/react-in-jsx-scope */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {Text} from 'react-native';
+
+const App = () => {
+  return <Text>Hello</Text>;
+};
+
+export default App;
