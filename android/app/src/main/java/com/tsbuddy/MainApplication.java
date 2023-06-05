d1a441b1dd1e07d2ebb7777b6444fe2f34ae48eb
Utkarsh gupta <Utkarsh16071998@gmail.com>
2023-06-05 18:37:06 +0530

    main.java....

--- android/app/src/main/java/com/tsbuddy/MainApplication.java
+++ android/app/src/main/java/com/tsbuddy/MainApplication.java
@@ -8,9 +8,11 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.tsbuddy.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
